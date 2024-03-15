3ef5c71c723dc9bf1c06dc4cd327b8c27056b626
Кириенко Денис <kirden04@gmail.com>
2024-03-15 19:53:39 +1000

    lab1 final

--- lab1/main.py
+++ lab1/main.py
@@ -1,25 +1,22 @@
 import numpy as np
 from sympy import cos, diff, lambdify, symbols
 
+
+def div_sub_2(x1, x2, f):
+    return (f(x2) - f(x1)) / (x2 - x1)
+
+
+def div_sub_3(x1, x2, x3, f):
+    return (div_sub_2(x2, x3, f) - div_sub_2(x1, x2, f)) / (x3 - x1)
+
+
+def f(x):
+    return x / 2 - cos(x / 2)
+
+
 x, y = symbols("x y")
-
-var = 24
-
-if var == 24:
-
-    def f(x):
-        return x / 2 - cos(x)
-
-    nums = [0.64, 0.42, 0.87, 0.63]
-    a, b = 0.4, 0.9
-
-elif var == 15:
-
-    def f(x):
-        return 2 * x - cos(x)
-
-    nums = [0.44, 0.13, 0.58, 0.37]
-    a, b = 0.1, 0.6
+nums = [0.64]
+a, b = 0.4, 0.9
 
 
 steps = 10
@@ -28,16 +25,16 @@ points = [round(a + h * i, 10) for i in range(steps + 1)]
 
 result = []
 
+i = 0
+for point in points:
+    i += 1
+    print(i, point, f(point))
 
-def div_sub_2(x1, x2, f):
-    return f(x2) - f(x1) / (x2 - x1)
-
-
-def div_sub_3(x1, x2, x3, f):
-    return (div_sub_2(x2, x3, f) - div_sub_2(x1, x2, f)) / (x3 - x1)
-
+print("---------")
 
 for num in nums:
+    value = f(num)
+
     for i in range(len(points) - 1):
         if num >= points[i] and num <= points[i + 1]:
             if i == 0:
@@ -58,9 +55,7 @@ for num in nums:
 
     # r1
 
-    r1_func = lambdify(
-        x, expr=(0.5 * diff(f(x), x, 2)) * (num - ran[1]) * (num - ran[2])
-    )
+    r1_func = lambdify(x, expr=(0.5 * diff(f(x), x, 2)) * (x - ran[1]) * (x - ran[2]))
 
     local_interv = np.arange(ran[1], ran[2], 10e-6)
     vals = []
@@ -68,10 +63,12 @@ for num in nums:
     for point in local_interv:
         vals.append(r1_func(point))
 
-    r1 = f(num) - l1
+    r1 = l1 - f(num)
 
     r1_min = min(vals)
     r1_max = max(vals)
+
+    print(f"r1_max: {r1_max}", f"r1_min: {r1_min}", f"r1: {r1}", "\n")
 
     if not (r1 > r1_min and r1 <= r1_max):
         print(f"{num} error. r1 inequality is not correct: {r1_min, r1, r1_max}")
@@ -97,8 +94,7 @@ for num in nums:
 
     r2_func = lambdify(
         x,
-        expr=(1 / 6 * diff(f(x), x, 3))
-        * ((num - ran[0]) * (num - ran[1]) * (num - ran[2])),
+        expr=(1 / 6 * diff(f(x), x, 3)) * ((x - ran[0]) * (x - ran[1]) * (x - ran[2])),
     )
 
     local_interv = np.arange(ran[0], ran[2], 10e-6)
@@ -107,10 +103,12 @@ for num in nums:
     for point in local_interv:
         vals.append(r2_func(point))
 
-    r2 = f(num) - l2
+    r2 = l2 - f(num)
 
     r2_min = min(vals)
     r2_max = max(vals)
+
+    print(f"r2_max: {r2_max}", f"r2_min: {r2_min}", f"r2: {r2}", "\n")
 
     if not (r2 > r2_min and r2 <= r2_max):
         print(f"{num}: error. r2 inequality is not correct: {r2_min, r2, r2_max}")
@@ -131,7 +129,7 @@ for num in nums:
 
     num_res = {
         num: {
-            "f(num)": f(num),
+            "f(num)": value,
             "l1": l1,
             "r1": r1,
             "n1": n1,
@@ -142,5 +140,16 @@ for num in nums:
     }
 
     result.append(num_res)
+
+    val_4 = round(value, 5)
+    val_5 = round(value, 6)
+
+    l1_4 = round(l1, 5)
+    l2_5 = round(l2, 6)
+
+    print(f"val: {val_4}", f"l1: {l1_4}", f"sub: {abs(val_4-l1_4)}")
+    print(f"val: {val_5}", f"l2: {l2_5}", f"sub: {abs(val_5-l2_5)}\n")
+
+    print(f"n1-l1: {abs(n1-l1)} n2-l2: {abs(n2-l2)}")
 
     print("\n", num_res, "\n" + "---------")
